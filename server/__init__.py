d0408d3258e37aba8cb8c81cbdd33bf298c8d453
Adrian Cruz <adr.ca@live.com>
2022-12-16 16:47:23 -0500

    Add firebase login ui

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from server.firebase import db
 
 app = Flask(__name__)
@@ -6,4 +6,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello, World!</h1>"
+    return render_template("index.html")
