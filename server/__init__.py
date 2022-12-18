4f4f9fffb5b03803c87a6b862c4093ac767ef4f3
Adrian Cruz <adr.ca@live.com>
2022-12-18 00:16:02 -0500

    add more routes

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from server.firebase import db
 
 app = Flask(__name__)
@@ -7,3 +7,22 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/create", methods=["POST"])
+def create():
+    data = request.get_json()
+    doc = db.collection("posts").document()
+    doc.set(data)
+
+    userdoc = db.collection("users").document(data["user"]["uid"])
+    userdoc.set(data["user"])
+
+    return jsonify(data)
+
+
+@app.route("/read/<uid>")
+def read(uid):
+    posts = db.collection("posts").where("user.uid", "==", uid)
+    posts_list = [post.to_dict() for post in posts.stream()]
+    return jsonify(posts_list)
