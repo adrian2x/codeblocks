071d944568ec8008d4a1a94a3b4af71dbfcfcd9b
Adrian Cruz <adr.ca@live.com>
2022-12-21 18:03:01 -0500

    update routes

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,5 @@
 from server.firebase import firestore
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from server.firebase import db
 
 app = Flask(
@@ -10,43 +10,39 @@ app = Flask(
 )
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/posts/", methods=["POST"])
+@app.route("/api/posts/", methods=["POST"])
 def posts():
     "Create a post"
     data = request.get_json()
     doc = db.collection("posts").document()
     doc.set(data)
+    doc.update({"id": doc.id})
+    return jsonify(doc.get().to_dict())
 
-    return jsonify(data)
 
-
-@app.route("/posts/", methods=["GET"])
+@app.route("/api/posts/", methods=["GET"])
 def read_posts_uid():
     "Retrieve all posts created by a user id"
+    posts = db.collection("posts")
+
     uid = request.args.get("uid")
-    posts = (
-        db.collection("posts")
-        .where("user_id", "==", uid)
-        .order_by("created", direction=firestore.Query.DESCENDING)
-        .stream()
-    )
+    if uid:
+        posts = posts.where("user_id", "==", uid)
+
+    posts = posts.order_by("created", direction=firestore.Query.DESCENDING).stream()
     list_of_posts = [post.to_dict() for post in posts]
+
     return jsonify(list_of_posts)
 
 
-@app.route("/posts/<post_id>", methods=["GET"])
+@app.route("/api/posts/<post_id>", methods=["GET"])
 def read_post(post_id):
     "Retrieve post by post id"
     post = db.collection("posts").document(post_id).get().to_dict()
     return jsonify(post)
 
 
-@app.route("/posts/<post_id>", methods=["POST"])
+@app.route("/api/posts/<post_id>", methods=["POST"])
 def update_post(post_id):
     "Update post by post id"
     data = request.get_json()
@@ -56,7 +52,7 @@ def update_post(post_id):
     return jsonify(updated_doc)
 
 
-@app.route("/posts/<post_id>", methods=["DELETE"])
+@app.route("/api/posts/<post_id>", methods=["DELETE"])
 def delete_post(post_id):
     "Delete post by post id"
     data = request.get_json()
@@ -65,7 +61,7 @@ def delete_post(post_id):
     return jsonify(data)
 
 
-@app.route("/users/", methods=["POST"])
+@app.route("/api/users/", methods=["POST"])
 def users():
     "Create a user"
     data = request.get_json()
@@ -74,14 +70,14 @@ def users():
     return jsonify(data)
 
 
-@app.route("/users/<uid>", methods=["GET"])
+@app.route("/api/users/<uid>", methods=["GET"])
 def read_user(uid):
     "Retrieve user by user id"
     user = db.collection("users").document(uid).get().to_dict()
     return jsonify(user)
 
 
-@app.route("/users/<uid>", methods=["POST"])
+@app.route("/api/users/<uid>", methods=["POST"])
 def update_user(uid):
     "Update user by user id"
     data = request.get_json()
@@ -90,10 +86,18 @@ def update_user(uid):
     return jsonify(data)
 
 
-@app.route("/users/<uid>", methods=["DELETE"])
+@app.route("/api/users/<uid>", methods=["DELETE"])
 def delete_user(uid):
     "Delete user by user id"
     data = request.get_json()
     doc = db.collection("users").document(uid)
     doc.delete()
     return jsonify(data)
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<string:path>")
+@app.route("/<path:path>")
+def catch_all(path):
+    print("request", path)
+    return app.send_static_file("index.html")
