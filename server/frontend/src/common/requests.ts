5183abb91536e83cfa79d2023a703ca984570970
Adrian Cruz <adr.ca@live.com>
2022-12-21 18:07:46 -0500

    use api requests path

--- server/frontend/src/common/requests.ts
+++ server/frontend/src/common/requests.ts
@@ -1,4 +1,8 @@
-export function request(endpoint: string, body: any = undefined, customConfig: any = {}) {
+export function request<TResponse = {}>(
+  endpoint: string,
+  body: any = undefined,
+  customConfig: any = {}
+) {
   const config: any = {
     method: body ? 'POST' : 'GET',
     ...customConfig,
@@ -10,19 +14,37 @@ export function request(endpoint: string, body: any = undefined, customConfig: a
   if (body) {
     config.body = JSON.stringify(body)
   }
-  return window.fetch(endpoint, config).then((response) => response.json())
+  return window.fetch(endpoint, config).then((response) => response.json() as TResponse)
+}
+
+export interface TPost {
+  id?: string
+  title: string
+  description: string
+  created: number
+  code: string
+  language: string
+  user_id: string
 }
 
 export function getPost(id: string) {
-  return request(`/posts/${id}`)
+  return request<TPost>(`/api/posts/${id}`)
 }
 
-export function getPosts(id: string) {
-  return request(`/posts/${id}`)
+export function getPosts(uid?: string) {
+  let params = new URLSearchParams()
+  if (uid) params.set('uid', uid)
+  return request(`/api/posts/?${params.toString()}`)
 }
 
-export async function updatePost(post: any) {
+export async function createPost(post: any) {
   // Make a request with the post content
-  let data = await request('/posts/', post)
+  let data = await request<TPost>('/api/posts/', post)
+  return data
+}
+
+export async function updatePost(id: string, post: Partial<TPost>) {
+  // Make a request with the post content
+  let data = await request<TPost>(`/api/posts/${id}`, post)
   return data
 }
