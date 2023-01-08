249089e4da8603a0fa0412b567cae2d6e355b0ea
Rolando Diaz Cruz <rolandoo0@icloud.com>
2023-01-07 19:00:59 -0500

    Added type hinting to models

--- server/models/post.py
+++ server/models/post.py
@@ -2,14 +2,14 @@ from server.firebase import db
 
 
 class Post:
-    def __init__(self, pid) -> None:
+    def __init__(self, pid: str) -> None:
         self.doc = db.collection("posts").document(pid)
 
     def get(self):
         "Get a post document"
         return self.doc.get()
 
-    def update(self, data):
+    def update(self, data: dict):
         "Update a post document"
         self.doc.update(data)
         return self.doc.get()
--- server/models/user.py
+++ server/models/user.py
@@ -2,14 +2,14 @@ from server.firebase import db
 
 
 class User:
-    def __init__(self, uid) -> None:
+    def __init__(self, uid: str) -> None:
         self.doc = db.collection("users").document(uid)
 
     def get(self):
         "Get a user document"
         return self.doc.get()
 
-    def update(self, data):
+    def update(self, data: dict):
         "Update a user document"
         self.doc.update(data)
         return self.doc.get()
