bd50995ee05939733b585a703f1e0343c8a1aab5
Adrian Cruz <adr.ca@live.com>
2023-01-12 16:30:58 -0500

    fix react typedefs build

--- server/frontend/vite.config.ts
+++ server/frontend/vite.config.ts
@@ -5,7 +5,8 @@ import preact from '@preact/preset-vite'
 export default defineConfig({
   resolve: {
     alias: {
-      react: 'preact/compat'
+      react: 'preact/compat',
+      'react-dom': 'preact/compat'
     }
   },
   plugins: [preact(), splitVendorChunkPlugin()],
