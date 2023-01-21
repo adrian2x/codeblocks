cc294452a21423aef9f2fd0161f6eca49cd56746
Adrian Cruz <adr.ca@live.com>
2023-01-21 02:02:54 -0500

    update routes

--- server/frontend/src/main.tsx
+++ server/frontend/src/main.tsx
@@ -18,6 +18,10 @@ const router = createBrowserRouter([
         element: <PostsContainer />
       },
       {
+        path: 'explore',
+        element: <PostsContainer key='explore' />
+      },
+      {
         path: 'post',
         element: <Post key={Math.random()} />
       },
