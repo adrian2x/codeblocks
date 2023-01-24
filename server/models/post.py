3ee6c127f41b8e58bcf9dd34b2a84e7a940b3cb3
Rolando Diaz Cruz <rolandoo0@icloud.com>
2023-01-24 17:23:08 -0500

    Added saved posts retrieval

--- server/models/post.py
+++ server/models/post.py
@@ -34,6 +34,28 @@ class Post:
         posts = posts.limit(limit).stream()
         return [post.to_dict() for post in posts]
 
+    @staticmethod
+    def get_saved_posts_by_user_id(uid: str, cursor=None, limit: int = 10):
+        "Retrieve all posts saved by a user id"
+        posts = db.collection("user_posts").document(uid).collection("saved")
+
+        # Sort by created at
+        posts = posts.order_by("created", direction=firestore.Query.DESCENDING)
+
+        # Set the page offset
+        if cursor:
+            pagination_doc = (
+                db.collection("user_posts")
+                .document(uid)
+                .collection("saved")
+                .document(cursor)
+                .get()
+            )
+            posts = posts.start_after(pagination_doc)
+
+        posts = posts.limit(limit).stream()
+        return [post.to_dict() for post in posts]
+
     def get(self):
         "Get a post document"
         return self.doc.get()
