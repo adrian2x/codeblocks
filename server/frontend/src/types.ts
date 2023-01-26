dfd5fccc14e02f6551ae8da3d709fa05cafc8896
Adrian Cruz <adr.ca@live.com>
2023-01-25 23:17:39 -0500

    update get user response

--- server/frontend/src/types.ts
+++ server/frontend/src/types.ts
@@ -6,3 +6,32 @@ export type RouteProps = {
 }
 
 export type FirebaseUser = firebase.User | null
+
+export interface TPost {
+  id: string
+  title: string
+  description: string
+  created: number
+  code: string
+  windowTitle?: string
+  language: string
+  theme: string
+  user: {
+    id: string
+    uid: string
+    displayName: string
+    displayHandle: string
+    photoUrl: string | null
+  }
+  preview?: string
+}
+
+export type TUser = {
+  id: string
+  photoUrl: string
+  photoURL: string
+  displayName: string
+  displayHandle: string
+  backgroundColor?: string
+  about?: string
+}
